bdb33ad5966931abe893fe95bff1ca4c0f894b28
Samson Offorjindu <126519078+korisnik-samson@users.noreply.github.com>
2024-07-04 23:27:55 +0200

    Modified app.test.ts

--- tests/server.test.ts
+++ tests/server.test.ts
@@ -1,56 +1,25 @@
-// __tests__/app.test.ts
+import { transformData } from '../src/api/transformData';
+import { ApiResponse, TransformedData } from '../src/types';
 
-import request from 'supertest';
-import { app } from '../src/server'; // Adjust the path as needed
-import * as transformData from '../src/api/transformData';
-import axios from 'axios';
+describe('transformData', () => {
+    it('should transform data correctly', () => {
+        const apiResponse: ApiResponse = {
+            items: [
+                { fileUrl: 'http://192.168.0.1/path/to/file1.txt' },
+                { fileUrl: 'http://192.168.0.1/path/to/file2.txt' },
+                { fileUrl: 'http://192.168.0.2/path/to/file3.txt' },
+            ],
+        };
 
-// Mock axios
-jest.mock('axios');
-const mockedAxios = axios as jest.Mocked<typeof axios>;
+        const expectedOutput: TransformedData = {
+            '192.168.0.1': [
+                { path: ['path', 'to', 'file1.txt'] },
+                { path: ['path', 'to', 'file2.txt'] },
+            ],
+            '192.168.0.2': [{ path: ['path', 'to', 'file3.txt'] }],
+        };
 
-// Mock the initCache function
-jest.spyOn(transformData, 'initCache').mockImplementation(async() => {
-    const mockData = { key: 'value' };
-    transformData. = mockData;
-    transformData.cacheTimestamp = Date.now();
-});
-
-// Initialize the app before running tests
-beforeAll(async() => {
-    await transformData.initCache();
-});
-
-describe('GET /api/files', () => {
-    beforeEach(() => {
-        transformData.cachedData = null;
-        transformData.cacheTimestamp = 0;
-    });
-
-    it('should return data when cache is initialized', async() => {
-        const mockData = { key: 'value' };
-        transformData.cachedData = mockData;
-        transformData.cacheTimestamp = Date.now();
-
-        const response = await request(app).get('/api/files');
-        expect(response.status).toBe(200);
-        expect(response.body).toEqual(mockData);
-    });
-
-    it('should return an error if cache is not initialized', async() => {
-        const response = await request(app).get('/api/files');
-        expect(response.status).toBe(503);
-        expect(response.body).toEqual({
-            error: 'Service unavailable. Please try again later.'
-        });
-    });
-
-    it('should fetch data from external API and set cache', async() => {
-        const mockData = { key: 'value' };
-        mockedAxios.get.mockResolvedValueOnce({ data: mockData });
-
-        await transformData.initCache();
-        const cachedData = transformData.cachedData;
-        expect(cachedData).toEqual(mockData);
+        const result = transformData(apiResponse);
+        expect(result).toEqual(expectedOutput);
     });
 });
